4a49f80327f81f3f7e8b82101bc73df127dc9909
Amir Hosein Haji Mohammad rezaie <79265364+amir-haji@users.noreply.github.com>
2024-08-18 14:02:58 +0330

    Correct UnidirectionalState.java

--- Phase 1/src/main/java/graph/UnidirectionalState.java
+++ Phase 1/src/main/java/graph/UnidirectionalState.java
@@ -12,6 +12,10 @@ public class UnidirectionalState implements RouteState {
 
     @Override
     public void changeTimeUnit(Graph graph, int unit) {
-        System.out.println("You cannot change time unit in unindirected mode.");
+        for (Node node : graph.getGraph()) {
+            for (Edge edge : node.getEdges()) {
+                edge.setWeight(unit);
+            }
+        }
     }
 }
